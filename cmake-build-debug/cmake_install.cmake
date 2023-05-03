0bf36d534e65ef7ad8d0016c38a9d924166c123e
Vpert <amorikov95@gmail.com>
2023-05-03 17:13:42 +0300

    v0.3a fix

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/amoik/Desktop/project/graphico
+# Install script for directory: F:/hybrid projects/graphico
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/fav")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Graphico")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/sfml_clion/i686-7.3.0-release-posix-dwarf-rt_v5-rev0/mingw32/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/amori/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/amoik/Desktop/project/graphico/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/hybrid projects/graphico/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
