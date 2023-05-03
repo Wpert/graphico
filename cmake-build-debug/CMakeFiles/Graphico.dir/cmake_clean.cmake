a9733df40de47864aa8772db7b406d77c15411b7
Vpert <amorikov95@gmail.com>
2023-05-03 17:18:10 +0300

    v0.3a fix

---
+++ cmake-build-debug/CMakeFiles/Graphico.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graphico.dir/algo/algos.cpp.obj"
+  "CMakeFiles/Graphico.dir/algo/algos.cpp.obj.d"
+  "CMakeFiles/Graphico.dir/buttons/gui.cpp.obj"
+  "CMakeFiles/Graphico.dir/buttons/gui.cpp.obj.d"
+  "CMakeFiles/Graphico.dir/buttons/tbuttons.cpp.obj"
+  "CMakeFiles/Graphico.dir/buttons/tbuttons.cpp.obj.d"
+  "CMakeFiles/Graphico.dir/buttons/textbox.cpp.obj"
+  "CMakeFiles/Graphico.dir/buttons/textbox.cpp.obj.d"
+  "CMakeFiles/Graphico.dir/graphs/tgraph.cpp.obj"
+  "CMakeFiles/Graphico.dir/graphs/tgraph.cpp.obj.d"
+  "CMakeFiles/Graphico.dir/graphs/tvertex.cpp.obj"
+  "CMakeFiles/Graphico.dir/graphs/tvertex.cpp.obj.d"
+  "CMakeFiles/Graphico.dir/main.cpp.obj"
+  "CMakeFiles/Graphico.dir/main.cpp.obj.d"
+  "Graphico.exe"
+  "Graphico.exe.manifest"
+  "Graphico.pdb"
+  "libGraphico.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graphico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
